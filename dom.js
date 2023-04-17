0aeea4f8af3c28a242ec92c7dd43436de0da6e26
runtyalien <omgadkar@gmail.com>
2023-04-17 18:02:54 +0530

    font weight

--- dom.js
+++ dom.js
@@ -1,7 +1,17 @@
 var headerTitle = document.getElementById('header-title');
 var titleElements = document.getElementsByClassName('title');
+var items = document.getElementsByClassName('list-group-item');
+
 headerTitle.style.border = "4px solid black";
 for (var i = 0; i < titleElements.length; i++) {
     titleElements[i].style.fontWeight = "bold";
     titleElements[i].style.color = "green";
-  }
+ }
+
+items[2].style.backgroundColor = 'green';
+
+    items[0].style.fontWeight = "bold";
+    items[1].style.fontWeight = "bold";
+    items[2].style.fontWeight = "bold";
+    items[3].style.fontWeight = "bold";
+ 
